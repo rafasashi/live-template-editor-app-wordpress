909a57502e72fff1edc08199d338aa1682748793
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2018-01-21 16:16:04 +0545

    css & js removed

--- includes/class-ltple.php
+++ includes/class-ltple.php
@@ -91,7 +91,7 @@ class LTPLE_App_Wordpress {
 	} // End __construct ()
 	
 	public function register_app_type(){
-
+		
 		$apps = $this->parent->apps->get_terms( 'app-type', array(
 
 			'wordpress' => array(
@@ -202,8 +202,8 @@ class LTPLE_App_Wordpress {
 	 */
 	public function enqueue_styles () {
 		
-		wp_register_style( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'css/frontend.css', array(), $this->_version );
-		wp_enqueue_style( $this->_token . '-frontend' );
+		//wp_register_style( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'css/frontend.css', array(), $this->_version );
+		//wp_enqueue_style( $this->_token . '-frontend' );
 	} // End enqueue_styles ()
 
 	/**
@@ -214,8 +214,8 @@ class LTPLE_App_Wordpress {
 	 */
 	public function enqueue_scripts () {
 		
-		wp_register_script( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'js/frontend' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
-		wp_enqueue_script( $this->_token . '-frontend' );
+		//wp_register_script( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'js/frontend' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		//wp_enqueue_script( $this->_token . '-frontend' );
 	} // End enqueue_scripts ()
 
 	/**
@@ -226,8 +226,8 @@ class LTPLE_App_Wordpress {
 	 */
 	public function admin_enqueue_styles ( $hook = '' ) {
 		
-		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
-		wp_enqueue_style( $this->_token . '-admin' );
+		//wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
+		//wp_enqueue_style( $this->_token . '-admin' );
 	} // End admin_enqueue_styles ()
 
 	/**
@@ -238,8 +238,8 @@ class LTPLE_App_Wordpress {
 	 */
 	public function admin_enqueue_scripts ( $hook = '' ) {
 		
-		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
-		wp_enqueue_script( $this->_token . '-admin' );
+		//wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		//wp_enqueue_script( $this->_token . '-admin' );
 	} // End admin_enqueue_scripts ()
 
 	/**
